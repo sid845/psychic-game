c296b4ca82cdd005260bc51604f3cff260a2942f
unknown <siddharthpatel845@gmail.com>
2017-05-15 13:01:12 -0400

    getting there

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+var wins=0;
+	var losses=0;
+	var gleft=9;
+	var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	var guess=letters[Math.floor(Math.random() * 25)];
+	// document.getElementById("demo1").innerHTML= guess;
+	
+	document.onkeyup=function(event)
+	{
+		var userGuess=event.key;
+		var guesses=[ ];
+		guesses.push(userGuess);
+		var letters=document.getElementById("demo").innerHTML += guesses+",";
+		
+		if(guess === userGuess) {
+			
+			wins++;
+            document.getElementById("win").innerHTML = wins;
+			gleft=9;
+			guesses.length=0;
+			document.getElementById("demo").innerHTML =" ";
+			guess=letters[Math.floor(Math.random() * 25)];
+			// document.getElementById("demo1").innerHTML= guess;
+		}
+		if(guess != userGuess) {
+			gleft--;
+			document.getElementById("gleft").innerHTML = gleft;
+			if(gleft<1)
+			{
+				losses++;
+				document.getElementById("loss").innerHTML = losses;
+				gleft=9;
+				guesses.length=0;
+				document.getElementById("demo").innerHTML =" ";
+				guess=letters[Math.floor(Math.random() * 25)];
+				// document.getElementById("demo1").innerHTML= guess;
+			}
+		}
+    };
